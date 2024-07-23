5de5567753ce3af4426eaf82cfe4e7bfd8160888
Dylan Gray <dylan.gray@revenuepathgroup.com>
2024-07-23 14:31:07 -0400

    got responsibility and accountability course extracted

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import fitz
+
+def scrape_pdf(pdf):
+    pdf_doc = fitz.open(pdf)
+    text = ''
+
+    for page_num in range(len(pdf_doc)):
+        page = pdf_doc.load_page(page_num)
+        text += page.get_text('text') + '\n'
+
+    text = text.replace('CONTINUE', '')
+
+    return text
+
+pdf = 'files/rise.pdf'
+extracted_text = scrape_pdf(pdf)
+
+with open('extracted_text.txt', 'w', encoding='utf-8') as file:
+    file.write(extracted_text)
